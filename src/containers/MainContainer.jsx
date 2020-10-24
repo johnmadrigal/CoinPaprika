e767a71c408de0c1953f520ef074914cb842c876
John Madrigal <johnmadrigal17@gmail.com>
2020-10-24 15:40:21 -0700

    toggle left and right

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 const MainContainer = () => {
   const [top10Coins, setTop10] = useState([])
   const [left, setLeft] = useState(null)
+  const [right, setRight] = useState(null)
   const [data, setData] = useState([]);
   
   useEffect( () => {
@@ -10,14 +11,20 @@ const MainContainer = () => {
       const response = await fetch('https://api.coinpaprika.com/v1/coins')
       const json = await response.json()
       const top10 = json.slice(0, 9);
+      setLeft(top10[0].name)
+      setRight(top10[0].name)
       setTop10(top10.map( coin => <option key={coin.id}>{coin.name}</option>))
     }
     fetching()
   }, [])
 
   const onSelect = (e) => {
-    console.log('toggle select', e.target.value)
+    if(e.target.id === "left") setLeft(e.target.value) 
+    if(e.target.id === "right") setRight(e.target.value) 
+    console.log('toggle select', e.target.id)
   }
+
+  console.log('left', left)
 
   return (
   <main>
@@ -27,7 +34,7 @@ const MainContainer = () => {
     <select id="right" onChange={onSelect}>
       {top10Coins}
     </select>
-    <h1>1 </h1>
+    <h1>1 {left} = {right}</h1>
   </main>  );
 }
  
