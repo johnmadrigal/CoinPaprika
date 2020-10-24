78421f5bf25b256872f128361b4e9b322e8997bd
John Madrigal <johnmadrigal17@gmail.com>
2020-10-24 12:34:41 -0700

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <select>
+        <option value="Bitcoin">Bitcoin</option>
+      </select>
+      <select>
+        <option value="Bitcoin">Ethereum</option>
+      </select>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
