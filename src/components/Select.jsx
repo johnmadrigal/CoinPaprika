3daa4a6d56621696e8f9ea6a9999a30abac386aa
John Madrigal <johnmadrigal17@gmail.com>
2020-10-24 17:53:30 -0700

    default selection to bitcoin and ethereum

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Select = (props) => {
-  const { coins } = props;
+  const { id, options, value, name, set, onSelect } = props;
+  const selections = options.map( (option) => {
+    if(option[value] === set) return <option id={option[name]} value={option[value]} selected>{option[name]}</option>
+    return <option id={option[name]} value={option[value]} >{option[name]}</option>
+  })
+  
   return (
-    <select>
-      <option value="Bitcoin">Bitcoin</option>
+    <select id={id} onChange={onSelect}>
+      {selections}
     </select>
     );
 }
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,33 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import Select from '../components/Select'
 
 //most likely want to either cash these request or grab them on select
 const MainContainer = () => {
   const [loading, setLoading] = useState(true)
-  const [top10Coins, setTop10] = useState([])
-  const [tickers, setTickers] = useState([])
-  const [left, setLeft] = useState(null)
-  const [right, setRight] = useState(null)
   const [data, setData] = useState({}) //{id: null, quotes: { ETH: {price: 0}}}
   const [price, setPrice] = useState(null)
+  const [left, setLeft] = useState('');
+  const [right, setRight] = useState('');
+  const [coins, setCoins] = useState([{id: 'btc', name: 'Bitcoin'}, {id: 'eth', name: "Ethereum"}])
+
   
   // const url = `https://api.coinpaprika.com/v1/tickers/${left}?quotes=${right}`
   const url = `https://api.coinpaprika.com/v1/tickers/btc-bitcoin?quotes=ETH`
   //https://api.coinpaprika.com/v1/tickers/btc-bitcoin?quotes=BTC
   useEffect( () => {
-    const fetching = async () => {
-      setLoading(!loading)
-      const response = await fetch('https://api.coinpaprika.com/v1/coins')
-      const json = await response.json()
-      const top10 = json.slice(0, 9);
-      console.log('top10', top10)
-      const res = await fetch(url)
-      const jsonRes = await res.json()
-      // console.log('jsonRes',jsonRes)
-      setLeft(top10[0])
-      setRight(top10[0])
-      setTop10(top10.map( coin => <option key={coin.id}>{coin.name}</option>))
-    }
-    // fetching()
     setLoading(!loading)
     fetch('https://api.coinpaprika.com/v1/tickers/btc-bitcoin?quotes=ETH')
       .then( res => res.json())
@@ -49,7 +36,7 @@ const MainContainer = () => {
     console.log('trigger data useeffect')
     if (checkNested(data, "quotes", "ETH", "price")) {
       const results = getNested(data, "quotes", "ETH", "price");
-      setPrice(results)
+      setPrice(results.toFixed(2))
       console.log('results', results)
     }
     // try{
@@ -58,28 +45,43 @@ const MainContainer = () => {
     // } catch (e) {
     //   console.log('error in useEffect nesting', e)
     // }
-  }, [data])
-
-  
+  }, [data])  
 
   const onSelect = (e) => {
+    console.log('onSelect', e.target.id)
+    console.log('onSelect', e.target.value)
+
     if(e.target.id === "left") setLeft(e.target.value);
     if(e.target.id === "right") setRight(e.target.value);
   }
 
-
   return loading ? <h1>Loading...</h1> : (
   <main>
-    <select id="left" onChange={onSelect}>
-      {/* {top10Coins} */}
+    <Select id="left" options={coins} name="name" value="id" onSelect={onSelect} set="btc"/>
+    <Select id="right" options={coins} name="name" value="id" onSelect={onSelect} set="eth"/>
+    {/* <select id="left" onChange={onSelect}>
       <option value="Bitcoin">Bitcoin</option>
     </select>
     <select id="right" onChange={onSelect}>
-      {/* {top10Coins} */}
       <option value="Bitcoin">Ethereum</option>
-    </select>
+    </select> */}
     <h1>1 Bitcoin = {price} Ethereum</h1>
   </main>  );
 }
  
 export default MainContainer;
+
+// const fetching = async () => {
+//   setLoading(!loading)
+//   const response = await fetch('https://api.coinpaprika.com/v1/coins')
+//   const json = await response.json()
+//   const top10 = json.slice(0, 9);
+//   console.log('top10', top10)
+//   const res = await fetch(url)
+//   const jsonRes = await res.json()
+//   // console.log('jsonRes',jsonRes)
+//   setLeft(top10[0])
+//   setRight(top10[0])
+//   setTop10(top10.map( coin => <option key={coin.id}>{coin.name}</option>))
+// }
+// fetching()
